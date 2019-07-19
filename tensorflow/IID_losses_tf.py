5ce6106243c5b950b26b4101fe8683aca9b1145f
Nathan Ing <ing.nathany@gmail.com>
2019-07-19 10:13:25 -0700

    Fix the loss
    https://github.com/astirn/IIC

--- tensorflow/IID_losses_tf.py
+++ tensorflow/IID_losses_tf.py
@@ -4,7 +4,11 @@ import tensorflow as tf
 import numpy as np
 from scipy.special import softmax
 from generate_clusterings import genclust
-  
+
+
+# Check this one:
+# https://github.com/astirn/IIC
+
 
 def IID_loss(x_out, x_tf_out, lamb=1.0, EPS=sys.float_info.epsilon):
   k = x_out.shape[-1]
@@ -13,18 +17,15 @@ def IID_loss(x_out, x_tf_out, lamb=1.0, EPS=sys.float_info.epsilon):
   # joint probability
   p = tf.reduce_sum(tf.expand_dims(x_out, 2) * tf.expand_dims(x_tf_out, 1), 0)
   p = (p + tf.transpose(p)) / 2 # symmetry
+  p = tf.clip_by_value(p, EPS, 1e9)
   p /= tf.reduce_sum(p) # normalize
 
-  pi = tf.broadcast_to(tf.reshape(tf.reduce_sum(p, axis=1), (k, 1)), (k, k))
-  pj = tf.broadcast_to(tf.reshape(tf.reduce_sum(p, axis=0), (1, k)), (k, k))
+  pi = tf.broadcast_to(tf.reshape(tf.reduce_sum(p, axis=0), (k, 1)), (k, k))
+  pj = tf.broadcast_to(tf.reshape(tf.reduce_sum(p, axis=1), (1, k)), (k, k))
 
-  p = tf.clip_by_value(p, EPS, 1e9)
-  pi = tf.clip_by_value(pi, EPS, 1e9)
-  pj = tf.clip_by_value(pj, EPS, 1e9)
-
-  iloss = tf.reduce_sum( - p * (tf.math.log(p) \
-                                - lamb * tf.math.log(pi) \
-                                - lamb * tf.math.log(pj) ))
+  iloss = -tf.reduce_sum(  
+    p * (tf.math.log(p) - lamb * tf.math.log(pi) - lamb * tf.math.log(pj) 
+  ))
 
   return iloss
 
